9880035d939a85f6d85d9733f583ef7a1a4115b4
Monsif-Seaton <monsifseatonwork@outlook.com>
2020-07-02 15:30:18 +0000

    fix to work without ansible

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,5 @@ sudo apt update -y
 sudo apt install python3 python3-pip -y
 pip3 install --user ansible
 ansible --version
-ansible-playbook -v -i inventory.cfg playbook.yml
+#ansible-playbook -v -i inventory.cfg playbook.yml
 
