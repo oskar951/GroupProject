1a0d0f495dbc912ba639fdb2614a7f678d4a0e0d
Monsif-Seaton <55064084+Monsif-Seaton@users.noreply.github.com>
2020-07-06 10:35:37 +0100

    added image updates

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo docker-compose up
+docker rmi $(docker images -aq) -f #delete all images
+
+docker-compose build
+
+docker push hsjhita1/backend:latest
+
+docker push hsjhita1/frontend:latest
+
+sudo docker-compose up #remove and replace with kubernetes commands
