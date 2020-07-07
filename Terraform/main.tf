d8a4432d4e058e7d3748d4c044bee9e347c03554
Ubuntu <ubuntu@ip-172-31-15-97.eu-west-2.compute.internal>
2020-07-07 16:15:08 +0000

    added cloudwatch

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                  = "eu-west-2"
+  region                  = "eu-west-1"
   shared_credentials_file = "/home/ubuntu/.aws/credentials"
 }
 
@@ -47,4 +47,17 @@ module "eks" {
 module "lambda" {
   source = "./LAMBDA"
   role   = module.aws_iam.lambda_role
+}
+
+module "sns" {
+  source          = "./SNS"
+  lambda_recovery = module.lambda.lambdaInstanceArn
+}
+
+module "cloudwatch" {
+  source                = "./CLOUDWATCH"
+  instanceID            = module.webserver_node.ec2_id
+  instance_recovery_arn = module.sns.recovery_arn
+  lambda_image_arn      = module.lambda.lambdaImageArn
+  lambda_snapshot_arn   = module.lambda.lambdaSnapshotArn
 }
