f3e93e2a03e57ad91c3c59656d8d3bee4b5acbf4
hsjhita1 <hjhita1997@gmail.com>
2020-07-08 11:38:24 +0100

    Jenkins udate

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,4 +8,7 @@
 #docker push hsjhita1/frontend:latest
 . ~/.bashrc
 printenv
+env URL=${DBURL}
+env DBUSER=${DBUSER}
+env DBPASS=${DBPASS}
 sudo docker-compose up -d #remove and replace with kubernetes commands
