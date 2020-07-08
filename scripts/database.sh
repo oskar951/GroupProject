a6a09290b35ad374bc433b18e568e9b3edbf61f1
hsjhita1 <hjhita1997@gmail.com>
2020-07-08 11:28:29 +0100

    Update env

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,4 +1,4 @@
 sudo apt install mysql-client -y
 . ~/.bashrc
-mysql --host=$dburl --user=$dbuser --password=$dbpass < /var/lib/jenkins/workspace/Group/spring-petclinic-rest/src/main/resources/db/mysql/initDB.sql
-mysql --host=$dburl --user=$dbuser --password=$dbpass "petclinic" < /var/lib/jenkins/workspace/Group/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
+mysql --host=$DBURL --user=$DBUSER --password=$DBPASS < /var/lib/jenkins/workspace/Group/spring-petclinic-rest/src/main/resources/db/mysql/initDB.sql
+mysql --host=$DBURL --user=$DBUSER --password=$DBPASS "petclinic" < /var/lib/jenkins/workspace/Group/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
