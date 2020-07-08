89646bd5925883a96ae7c67d6fa7b7b9d9bcafff
Ubuntu <ubuntu@ip-172-31-15-97.eu-west-2.compute.internal>
2020-07-08 08:55:51 +0000

    fixed tag issue

--- scripts/lambda/createInstance.py
+++ scripts/lambda/createInstance.py
@@ -1,6 +1,9 @@
 import boto3
 
 def lambda_handler(event, context):
+    currentDate = datetime.date.today()
+    stringDate = currentDate.strftime("%d/%m/%y")
+
     ec2_client = boto3.client('ec2')
     ec2_resource = boto3.resource('ec2')
 
@@ -10,23 +13,24 @@ def lambda_handler(event, context):
     )
     for image in images.all():
         allImages.append(image.id)
+    
+    instanceName = 'Webserver_Node BACKUP' + stringDate
 
     instance = ec2_resource.create_instances(
         ImageId = allImages[0],
         InstanceType = 't2.micro',
         KeyName = 'aws',
         MaxCount = 1,
-        MinCount = 1
-### Commented out due to error
-#        ,TagSpecification = [
-#            {
-#                'ResourceType' : 'instance',
-#                'Tags' : [
-#                    {
-#                        'Key' : 'Name',
-#                        'Value' : 'Replacement test-VM'
-#                    }
-#                ]
-#            }
-#        ]
+        MinCount = 1,
+        TagSpecification = [
+            {
+                'ResourceType' : 'snapshot',
+                'Tags' : [
+                {
+                    'Key' : 'Name',
+                    'Value' : instanceName
+                }
+                ]
+            }
+        ]
     )
