85b7b60961027655ccda4e73fc7c45536146b8ba
hsjhita1 <hjhita1997@gmail.com>
2020-07-09 13:39:45 +0100

    Test

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,4 +1,4 @@
 sudo apt install mysql-client -y
 . ~/.bashrc
-mysql --host=$DBURL --user=$DBUSER --password=$DBPASS < /var/lib/jenkins/workspace/Group/spring-petclinic-rest/src/main/resources/db/mysql/initDB.sql
-mysql --host=$DBURL --user=$DBUSER --password=$DBPASS "petclinic" < /var/lib/jenkins/workspace/Group/spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
+mysql --host=$DBURL --user=$DBUSER --password=$DBPASS < ./spring-petclinic-rest/src/main/resources/db/mysql/initDB.sql
+mysql --host=$DBURL --user=$DBUSER --password=$DBPASS "petclinic" < ./spring-petclinic-rest/src/main/resources/db/mysql/populateDB.sql
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #docker rmi $(docker images -aq) -f #delete all images
-
 #docker-compose build
-
+sudo docker build -t hsjhita1/backend:latest ./spring-petclinic-rest/
+sudo docker build -t hsjhita1/frontend:latest ./spring-petclinic-angular/
 #docker push hsjhita1/backend:latest
-
 #docker push hsjhita1/frontend:latest
 . ~/.bashrc
 printenv
